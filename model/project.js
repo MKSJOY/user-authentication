b481562f12abe3c0c87fec95eed2609393c06f0f
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-04-06 17:40:31 +0600

    Update project.js

--- model/project.js
+++ model/project.js
@@ -38,7 +38,7 @@ export const createProject = async (projectData) => {
   const sql = `INSERT INTO projects 
     (company_id, project_name, location, contact_number, project_start_date, approx_handover_date, 
     project_code, stage, project_type, status, logo, architect_drawing_file) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
   const params = [
     company_id,
