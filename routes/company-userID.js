26dc314a84ec3bff016259b1b3256066bd67d4a5
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-04-08 10:54:46 +0600

    Update company-userID.js

--- routes/company-userID.js
+++ routes/company-userID.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.get("/user/:id", getCompaniesByUser); // Get companies by user ID
 
-export const companyRoutes = router;
+export const companyUserRoutes = router;
