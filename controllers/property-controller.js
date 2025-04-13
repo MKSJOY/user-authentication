b68208f2447caac5bb15570e34fccceca7016648
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-04-13 14:35:23 +0600

    Update property-controller.js

--- controllers/property-controller.js
+++ controllers/property-controller.js
@@ -49,7 +49,7 @@ export const updateProperties = async (req, res) => {
 
         const updatedProperty = await getPropertyById(req.params.id); // Fetch the updated property
 
-        return res.status(200).json({ success: true, message: "Updated successfully" /*, property: updatedProperty*/ });
+        return res.status(200).json({ success: true, message: "Updated successfully" , property: updatedProperty });
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
     }
