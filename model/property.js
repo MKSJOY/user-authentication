3877bebe9758f3a2f7385b1c0e8f16e350435f8d
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-04-13 12:26:17 +0600

    Update property.js

--- model/property.js
+++ model/property.js
@@ -58,38 +58,7 @@ export const createProperty = async (data) => {
    return {
       status: 200,
       message: 'Added successfully',
-      property: {
-        id,
-        company_id,
-        land_property_name,
-        land_property_id,
-        upazila,
-        district,
-        mouza_number,
-        survey_category,
-        khatian_number,
-        cs_khatian,
-        rs_khatian,
-        sa_khatian,
-        bs_khatian,
-        mutation_khatian,
-        city_survey_khatian,
-        survey_location,
-        additional_documents: additional_documents || [],
-        owner_name,
-        phone_number,
-        present_address,
-        nid,
-        nid_file,
-        owner_photo,
-        tin_number,
-        tin_file,
-        land_area,
-        unit,
-        note,
-        reminder
-      }
-    };
+      return await query(sql, values);
   } catch (err) {
     console.error('Error creating property:', err); // Log the error for debugging
     return { status: 500, message: 'Internal Server Error', error: err.message }; // Propagate the error
