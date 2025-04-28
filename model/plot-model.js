3c3b49d5e7526d422d65626ab584774d4f991a8c
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-04-28 12:59:29 +0600

    Update plot-model.js

--- model/plot-model.js
+++ model/plot-model.js
@@ -1,13 +1,27 @@
 import { query } from "../config/database.js";
 
 // Create a new plot
+// Helper to generate plot_code
+const generatePlotCode = () => {
+  const randomPart = Math.random().toString(36).substring(2, 6).toUpperCase(); // 4 random characters
+  const timePart = Date.now().toString(36).slice(-3).toUpperCase(); // 3 characters from timestamp
+  return `PLT-${timePart}${randomPart}`;
+};
+
+// Create a new plot
 export const createPlot = async (plotData) => {
   const { company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name } = plotData;
+
+  const plot_code = generatePlotCode(); // Auto-generate plot_code
+
   return await query(
-    "INSERT INTO plots (company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-    [company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name]
+    `INSERT INTO plots 
+     (company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name, plot_code) 
+     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name, plot_code]
   );
 };
+
 
 // Get all plots
 export const getAllPlots = async () => {
@@ -25,7 +39,7 @@ export const updatePlot = async (id, updatedData) => {
   const { company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name } = updatedData;
   return await query(
     "UPDATE plots SET company_id = ?, plot_name = ?, plot_shape = ?, plot_area = ?, inventory_for_sale = ?, note = ?, land_id = ?, property_name = ? WHERE id = ?",
-    [company_id, plot_name, plot_shape, plot_area, inventory_for_sale, note, land_id, property_name, id]
+    [plot_name, plot_shape, plot_area, inventory_for_sale, note, property_id, property_name, id]
   );
 };
 
@@ -35,6 +49,6 @@ export const deletePlot = async (id) => {
 };
 
 // Get plots by property ID
-export const getPlotsByPropertyId = async (land_id) => {
-    return await query("SELECT * FROM plots WHERE land_id = ?", [land_id]);
+export const getPlotsByPropertyId = async (propertyId) => {
+    return await query("SELECT * FROM plots WHERE property_id = ?", [propertyId]);
   };
