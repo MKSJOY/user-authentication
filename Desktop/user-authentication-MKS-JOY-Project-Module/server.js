d1df55ef30bd561e7d8bedd15017085726c9131c
MKS JOY <mks.joy4@gmail.com>
2025-05-18 10:22:25 +0600

    Fix: Sync uuid dependency and lock file

---
+++ Desktop/user-authentication-MKS-JOY-Project-Module/server.js
@@ -0,0 +1,94 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { authRoutes } from "./routes/auth.js";
+import { projectRoutes } from "./routes/project.js";
+import errorHandler from "./middleware/errorHandler.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
+import { buildRoutes } from "./routes/build-site.js";
+import { clientRoutes } from "./routes/register-client.js";
+import { companyRoutes } from "./routes/company-route.js";
+import { getBuildingRoutes } from "./routes/getBuildingsByProjectName.js";
+import { stageRoutes } from "./routes/stage-routes.js";
+import { projectTypeRoutes } from "./routes/project-type.js";
+import { propertyRoutes } from "./routes/property.js"; // Import property routes
+import { plotRoutes } from "./routes/plot-routes.js";
+import { companyUserRoutes } from "./routes/company-userID.js";
+import { expenseRoutes } from "./routes/expense.js";
+import { saleFlatRoutes } from "./routes/create_sale_order_flat.js";
+import { saleLandRoutes } from "./routes/create_sale_order_land.js";
+import { addScrapProduct } from "./routes/add_scrap_product.js";
+import { scrapSale } from "./routes/scrap_sale.js";
+import { fixSerial } from "./routes/fix-serial-client.js";
+import { configRoutes } from "./routes/configuration_HRM.js";
+import { employeeRoutes } from "./routes/employee_HRM.js";
+import { changeDesignation } from "./routes/change_designation_HRM.js";
+import { overtime } from "./routes/overtime_HRM.js";
+import { salaryLogChange } from "./routes/salary_change_log_HRM.js";
+import { logsRoutes } from "./routes/logsRoutes.js";
+import { labourRoutes } from "./routes/labourRoutes.js";
+
+dotenv.config(); // Load .env variables at the top
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : [];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`Blocked by CORS: ${origin}`);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data (form data)
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/project", authMiddleware, projectRoutes);
+app.use("/api/building", authMiddleware, buildRoutes);
+app.use("/api/client", authMiddleware, clientRoutes);
+app.use("/api/company", authMiddleware, companyUserRoutes);
+app.use("/api/company", authMiddleware, companyRoutes);
+app.use("/api/buildings-project", authMiddleware, getBuildingRoutes);
+app.use("/api/stages", authMiddleware, stageRoutes);
+app.use("/api/", authMiddleware, propertyRoutes);
+app.use("/api/project-type", authMiddleware, projectTypeRoutes);
+app.use("/api/plots", authMiddleware, plotRoutes);
+app.use("/api/expenses", authMiddleware, expenseRoutes);
+app.use("/api/sale-order/flat", authMiddleware, saleFlatRoutes);
+app.use("/api/sale-order/land", authMiddleware, saleLandRoutes);
+app.use("/api/add-scrap/product", authMiddleware, addScrapProduct);
+app.use("/api/scrap-sale", authMiddleware, scrapSale);
+app.use("/api/clients", fixSerial);
+app.use("/api/HRM/configuration", authMiddleware, configRoutes);
+app.use("/api/HRM", authMiddleware, employeeRoutes);
+app.use("/api/HRM/employee/change-designation", authMiddleware, changeDesignation);
+app.use("/api/HRM/overtime", authMiddleware, overtime);
+app.use("/api/HRM/change-salary-log", authMiddleware, salaryLogChange);
+app.use("/api/HRM/logs", authMiddleware, logsRoutes);
+app.use("/api/HRM/labour", authMiddleware, labourRoutes);
+
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`MySQL Connected!`);
+});
